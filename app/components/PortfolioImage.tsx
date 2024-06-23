43c6e9a2be845a930822c200331afb82834cb759
dsa821 <dsa821@gmail.com>
2024-06-22 21:11:32 -0700

    add react-photo-view library

--- app/components/PortfolioImage.tsx
+++ app/components/PortfolioImage.tsx
@@ -1,17 +1,23 @@
-import Image from "next/image";
-import React from "react"
+import Image from 'next/image';
+import React from 'react';
+
+import { PhotoProvider, PhotoView } from 'react-photo-view';
 
 type Props = {
-	src: string;
-	alt: string;
-	width?: number;
-	height?: number;
-}
+  src: string;
+  alt: string;
+  width?: number;
+  height?: number;
+};
 
 export const PortfolioImage: React.FC<Props> = (props) => {
-	return (
-		<div className="flex justify-center">
-			<Image {...props} />
-		</div>
-	)
-}
+  return (
+    <PhotoProvider>
+      <div className='flex justify-center'>
+        <PhotoView src={props.src} >
+          <Image {...props} className='cursor-pointer' />
+        </PhotoView>
+      </div>
+    </PhotoProvider>
+  );
+};
