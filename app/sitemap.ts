b5776d034466b79121c55692e9c2fc1ed4b8da60
dsa821 <dsa821@gmail.com>
2024-06-24 11:29:28 -0700

    hide blog for now

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,17 +1,20 @@
-import { getBlogPosts } from 'lib/utils'
+import { getBlogPosts } from 'lib/utils';
 
-export const baseUrl = 'https://dsa-dev.vercel.app'
+export const baseUrl = 'https://dsa-dev.vercel.app';
 
 export default async function sitemap() {
-  let blogs = getBlogPosts().map((post) => ({
-    url: `${baseUrl}/blog/${post.slug}`,
-    lastModified: post.metadata.publishedAt,
-  }))
+  // let blogs = getBlogPosts().map((post) => ({
+  //   url: `${baseUrl}/blog/${post.slug}`,
+  //   lastModified: post.metadata.publishedAt,
+  // }))
 
   let routes = ['', '/blog'].map((route) => ({
     url: `${baseUrl}${route}`,
     lastModified: new Date().toISOString().split('T')[0],
-  }))
+  }));
 
-  return [...routes, ...blogs]
+  return [
+    ...routes,
+    // ...blogs
+  ];
 }
